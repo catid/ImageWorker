3d5f7a969bb4db93e4b726e46d9fddc034e2a1b3
catid <mrcatid@gmail.com>
2022-09-03 14:58:00 -0700

    Wip

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='latent-diffusion',
+    version='0.0.1',
+    description='',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'numpy',
+        'tqdm',
+    ],
